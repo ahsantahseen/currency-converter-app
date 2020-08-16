24f17018548c2b0b96d3f7c1dcd39add60337c55
Ahsan Tahseen <58149036+ahsantahseen@users.noreply.github.com>
2020-08-17 00:44:03 +0500

    Base Design Done

---
+++ src/Components/CurrencyConRow/CurrencyConverterRow.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const CurrencyConverterRow = () => {
+  return (
+    <div>
+      <input type="number"></input>
+      <select>
+        <option value="H1">H1</option>
+      </select>
+    </div>
+  );
+};
+
+export default CurrencyConverterRow;
--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./App.css";
+import CurrencyConverterRow from "../Components/CurrencyConRow/CurrencyConverterRow";
 
 function App() {
   return (
     <div className="App">
       <h1>Currency Converter</h1>
+      <CurrencyConverterRow />
+      =
+      <CurrencyConverterRow />
     </div>
   );
 }
