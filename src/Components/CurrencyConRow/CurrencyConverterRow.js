2a79cf2be50fa455f6faccf9d73ccb0ece18826d
Ahsan Tahseen <58149036+ahsantahseen@users.noreply.github.com>
2020-08-17 02:34:29 +0500

    Version 1.0

--- src/Components/CurrencyConRow/CurrencyConverterRow.js
+++ src/Components/CurrencyConRow/CurrencyConverterRow.js
@@ -2,11 +2,15 @@ import React from "react";
 import "./CurrencyConverterRow.css";
 
 const CurrencyConverterRow = (props) => {
-  console.log(props.options);
   return (
     <div>
-      <input className="input" type="number"></input>
-      <select className="select">
+      <input
+        className="input"
+        type="number"
+        value={props.Amount}
+        onChange={props.AmountOnChangeHandler}
+      ></input>
+      <select className="select" onChange={props.OptionsonChangeHandler}>
         {props.ops.map((Option) => (
           <option key={Option} value={Option}>
             {Option}
--- src/Containers/App.js
+++ src/Containers/App.js
@@ -5,22 +5,71 @@ import axios from "axios";
 
 function App() {
   const [currencyOptions, setcurrencyOptions] = useState([]);
-  console.log(currencyOptions);
+  const [currentCurrency, setcurrentCurrency] = useState();
+  const [convertedCurrency, setconvertedCurrency] = useState();
+  const [exchangeRate, setexchangeRate] = useState();
+  const [amount, setamount] = useState(1);
+  const [amountInFromCurrency, setamountInFromCurrency] = useState(true);
+
+  let currentAmount, convertedAmount;
+  if (amountInFromCurrency) {
+    currentAmount = amount;
+    convertedAmount = amount * exchangeRate;
+  } else {
+    convertedAmount = amount;
+    currentAmount = amount / exchangeRate;
+  }
 
   useEffect(() => {
     axios.get("https://api.exchangeratesapi.io/latest").then((response) => {
+      const firstCurrencyOption = Object.keys(response.data.rates)[0];
       setcurrencyOptions([
         response.data.base,
         ...Object.keys(response.data.rates),
       ]);
+      setcurrentCurrency(response.data.base);
+      setconvertedCurrency(firstCurrencyOption);
+      setexchangeRate(response.data.rates[firstCurrencyOption]);
     });
   }, []);
+  const currentAmountHandler = (e) => {
+    setamount(e.target.value);
+    setamountInFromCurrency(true);
+  };
+  const convertedAmountHandler = (e) => {
+    setamount(e.target.value);
+    setamountInFromCurrency(false);
+  };
+
+  useEffect(() => {
+    if (currentCurrency != null && convertedCurrency != null) {
+      axios
+        .get(
+          `https://api.exchangeratesapi.io/latest?base=${currentCurrency}&symbols=${convertedCurrency}`
+        )
+        .then((response) =>
+          setexchangeRate(response.data.rates[convertedCurrency])
+        );
+    }
+  }, [currentCurrency, convertedCurrency]);
   return (
     <div className="App">
       <h1>Currency Converter</h1>
-      <CurrencyConverterRow ops={currencyOptions} />
+      <CurrencyConverterRow
+        ops={currencyOptions}
+        selectedCurrency={currentCurrency}
+        OptionsonChangeHandler={(e) => setcurrentCurrency(e.target.value)}
+        Amount={currentAmount}
+        AmountOnChangeHandler={currentAmountHandler}
+      />
       <p className="equals">=</p>
-      <CurrencyConverterRow ops={currencyOptions} />
+      <CurrencyConverterRow
+        ops={currencyOptions}
+        selectedCurrency={convertedCurrency}
+        OptionsonChangeHandler={(e) => setconvertedCurrency(e.target.value)}
+        Amount={convertedAmount}
+        AmountOnChangeHandler={convertedAmountHandler}
+      />
     </div>
   );
 }
